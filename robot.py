d37e8a5022a930bd66b5bba2f11a3f553329f355
pancrates <pancrates@protonmail.com>
2021-04-21 13:04:49 +0100

    kinemtics sanity check

--- robot.py
+++ robot.py
@@ -1,28 +1,59 @@
 import numpy as np
-import math 
+import math
+
+
+def ROUND(a):
+   return int(a + 0.5)
+
 
 class Robot:
-    def __init__(self,position={x:0,y:0,phi:0}):
-        self.position = position
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.phi = 0
         self.distances = None
+        self.naigbours = []
         self.grid = np.zeros((1000,1000)) ## 0 unknown, -1 empty , 1 full
-        
-    
+
+
     def update_sensor_readings(self,readings):
         self.distances = readings
-    
+
     #def update_grid(self):
-    
-    
-    def tileCoords(cellSize, x, y):
-        return (math.floor(x / cellSize) + 1, math.floor(y / cellSize) + 1)
-    
-    
 
-    def ROUND(a):
-        return int(a + 0.5)
 
-    def drawDDA(x1,y1,x2,y2):
+    def update_naigbours(self,naigbours):
+        self.naigbours = naigbours
+
+
+    def get_robot_position(self):
+        return {"x":self.x,"y":self.y,"phi":self.phi}
+
+
+    def move(self):
+        cmd_vel =  self.wander()
+        dPosition = self.kinematics(cmd_vel["u"],cmd_vel["w"])
+        self.x = self.x + dPosition["dx"]
+        self.y = self.y + dPosition['dy']
+        self.phi = self.phi + dPosition["dPhi"]
+        if self.phi > np.pi:
+            self.phi = -2*np.pi + self.phi
+
+        if self.phi < -np.pi:
+            self.phi = 2*np.pi + self.phi
+
+    def kinematics(self,u,w):
+        dx = math.cos(w)*u
+        dy = math.sin(w)*u
+        dPhi = w
+        return {"dx":dx,"dy":dy,"dPhi":dPhi}
+
+    def wander(self):
+        u = np.random.uniform(low=-np.pi,high=np.pi)
+        w = np.random.uniform(low=-np.pi,high=np.pi)
+        return {"u":u,"w":w}
+
+    def drawDDA(self,x1,y1,x2,y2):
         x,y = x1,y1
         length = (x2-x1) if (x2-x1) > (y2-y1) else (y2-y1)
         dx = (x2-x1)/float(length)
@@ -36,8 +67,8 @@ class Robot:
 
 
 
+#r = Robot()
+#r.drawDDA(2,5,10,200)
 
-drawDDA(2,5,10,20)
 
 
-    
--- simulator.py
+++ simulator.py
@@ -1,5 +1,5 @@
-from .robot import Robot
-
+from robot import Robot
+import matplotlib.pyplot as plt
 
 class Terrain:
     def get_distance(self, x, y, phi):
@@ -13,9 +13,38 @@ class Sim:
         self.timeStep = 0.01
         self.robotList = []
 
-    def step():
-        
-        
-    
-    
-    
+
+
+
+class BlindSim():
+    def __init__(self):
+        self.dt = 0.1
+        self.robotList = [Robot()]
+
+    def step(self,robot):
+            start_internal_pos = robot.get_robot_position()
+            robot.move()
+            end_internal_pos = robot.get_robot_position()
+            print("Start ",start_internal_pos)
+            print("End ",end_internal_pos)
+            return end_internal_pos
+
+    def no_env_sim(self):
+        history = []
+        for n in range(20000):
+            for i,r in enumerate(self.robotList):
+                h = self.step(r)
+                history.append(h)
+        self.plot_path(history)
+
+    def plot_path(self,history):
+        xs = [h["x"] for h in history]
+        ys = [h["y"] for h in history]
+        fig, ax = plt.subplots()  # Create a figure containing a single axes.
+        ax.plot(xs, ys)  # Plot some data on the axes
+        plt.show()
+
+
+
+bsim = BlindSim()
+bsim.no_env_sim()
